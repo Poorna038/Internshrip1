35dfa2be2a3b49031a1b6b56e95fba17807b6d26
Poorna038 <96222550+Poorna038@users.noreply.github.com>
2025-11-29 21:29:37 +0530

    Update back.py

--- back.py
+++ back.py
@@ -178,7 +178,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:8000"],
+    allow_origins=["https://invoicesite.netlify.app/", "https://internshrip1.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
